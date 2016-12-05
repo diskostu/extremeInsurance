eecb1aff4f729a4c51484ea0704ae2fedd3e82ee
Volker Ohrem <Volker Ohrem@Internet-PC>
2016-12-05 17:25:17 +0100

    Additional Covers added.

--- src/main/java/com/axa/sesummit/codinggame/model/Cover.java
+++ src/main/java/com/axa/sesummit/codinggame/model/Cover.java
@@ -2,16 +2,27 @@ package com.axa.sesummit.codinggame.model;
 
 public enum Cover {
 
-	Basic("Basic");
+	Basic("Basic", 1.8);
 
-	private Cover(final String value) {
+	private Cover(final String value, Double amount) {
 		this.value = value;
+		this.amount = amount;
 	}
 
 	private String value;
 
+	private Double amount;
+
 	public String getValue() {
 		return value;
 	}
-	
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
 }
