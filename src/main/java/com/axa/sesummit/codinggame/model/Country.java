8cd7e8839e715fab87e3e4868a9cd8ca382c8e8c
diskostu <denny.kluge@gmail.com>
2016-12-05 18:07:59 +0100

    Country entity (bugfixes and test)

--- src/main/java/com/axa/sesummit/codinggame/model/Country.java
+++ src/main/java/com/axa/sesummit/codinggame/model/Country.java
@@ -1,7 +1,6 @@
 package com.axa.sesummit.codinggame.model;
 
 public enum Country {
-    // @formatter:off
     Estonia("EE", 1.3), Finland("FI", 0.8), Slovakia("SV", 0.7), Luxembourg("LU", 1.3), Croatia("HR", 1.3),
     Austria("AU", 0.9), Greece("GR", 0.6), Latvia("LV", 0.6), SouthAfrica("ZA", 1.6), DominicanRepublic("DO", 1.3),
     Ireland("IE", 1.1), Czechia("CZ", 1.2), Italy("IT", 1.2), Macedonia("MK", 1.6), Denmark("DK", 1.2), Bulgaria("BG", 1.1),
@@ -12,13 +11,14 @@ public enum Country {
     Qatar("QA", 1.6), Cyprus("CY", 1.6), Romania("RO", 1.3), Belgium("BE", 0.9), Thailand("TH", 1.6),
     Spain("ES", 1.1), DemocraticPeoplesRepublicOfKorea("KR", 6.9), Chad("TD", 1.3),
     Lithuania("LT", 0.7), WallisAndFutuna("WF", 1.5), Uruguay("UY", 1.6), Germany("DE", 0.8);
-    // @formatter:on
 
-    private double value;
     private String countryCode;
+    private double value;
 
-    Country(final double value, final String countryCode) {
+    Country(final String countryCode, final double value) {
+        this.countryCode = countryCode;
         this.value = value;
+
     }
 
     public double getValue() {
@@ -29,7 +29,7 @@ public enum Country {
         return countryCode;
     }
 
-    public double getValueForCode(final String code) {
+    public static double getValueForCode(final String code) {
         for (final Country country : Country.values()) {
             if (country.getCountryCode().equals(code)) {
                 return country.getValue();
