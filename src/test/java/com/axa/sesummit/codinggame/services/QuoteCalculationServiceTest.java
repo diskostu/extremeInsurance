ca9778f9beba23724df485c4c463ec2ad919d534
Volker Ohrem <Volker Ohrem@Internet-PC>
2016-12-05 17:11:22 +0100

    Fixed broken test

--- src/test/java/com/axa/sesummit/codinggame/services/QuoteCalculationServiceTest.java
+++ src/test/java/com/axa/sesummit/codinggame/services/QuoteCalculationServiceTest.java
@@ -1,5 +1,6 @@
 package com.axa.sesummit.codinggame.services;
 
+import com.axa.sesummit.codinggame.model.Cover;
 import com.axa.sesummit.codinggame.model.QuoteRequest;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,21 +9,22 @@ import java.util.Arrays;
 
 public class QuoteCalculationServiceTest {
 
-    /**
-     * The input is the same as in the first example here: https://github.com/arolla/Extreme-Insurance/blob/master/README.md
-     */
-    @Test
-    public void calculateQuote() throws Exception {
-        final String country = "DE";
-        final String departureDate = "2016-11-15";
-        final String returnDate = "2016-12-15";
-        final Integer[] travellerAges = {32, 39};
-        final String[] options = {"MedicalConditions"};
-        final String cover = "Basic";
+	/**
+	 * The input is the same as in the first example here:
+	 * https://github.com/arolla/Extreme-Insurance/blob/master/README.md
+	 */
+	@Test
+	public void calculateQuote() throws Exception {
+		final String country = "DE";
+		final String departureDate = "2016-11-15";
+		final String returnDate = "2016-12-15";
+		final Integer[] travellerAges = { 32, 39 };
+		final String[] options = { "MedicalConditions" };
 
-        final QuoteRequest request = new QuoteRequest(country, departureDate, returnDate, Arrays.asList(travellerAges), Arrays.asList(options), cover);
+		final QuoteRequest request = new QuoteRequest(country, departureDate, returnDate, Arrays.asList(travellerAges),
+				Arrays.asList(options), Cover.Basic);
 
-        final String result = new QuoteCalculationService().calculateQuote(request);
-        Assert.assertEquals("108.0", result);
-    }
+		final Double result = new QuoteCalculationService().calculateQuote(request);
+		Assert.assertEquals(Double.valueOf(108.0), result);
+	}
 }
