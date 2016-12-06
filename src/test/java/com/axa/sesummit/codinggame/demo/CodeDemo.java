53da0f37cbb03b6e297b841838a218812299921b
diskostu <denny.kluge@gmail.com>
2016-12-06 12:21:16 +0100

    more logging

---
+++ src/test/java/com/axa/sesummit/codinggame/demo/CodeDemo.java
@@ -0,0 +1,13 @@
+package com.axa.sesummit.codinggame.demo;
+
+/**
+ * Created by diskostu on 06.12.16.
+ */
+public class CodeDemo {
+
+    public static void main(String[] args) {
+        final int days = 9;
+
+        System.out.println(days % 7);
+    }
+}
